8ad2a642b4869c732a75a1c6fcfd75a992cf0825
iDigitalFlame <idf@idigitalflame.com>
2020-01-30 11:07:31 -0500

    Update License Files

--- beacons.py
+++ beacons.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python
-# Retrieves the active beacons from Scorebot based on the
-# supplied Team Token.
-# iDigitalFlame
-#
+# Retrieves the active beacons from Scorebot based on the supplied Team Token.
 # beacons.py <scorebot> <token>
+#
+# Copyright (C) 2020 iDigitalFlame
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 
 from requests import session
 from sys import exit, argv, stderr
